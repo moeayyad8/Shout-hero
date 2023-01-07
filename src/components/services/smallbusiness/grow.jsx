3c63b965612168f771800cab05c090f851239159
moeayyad8 <68765170+moeayyad8@users.noreply.github.com>
2023-01-07 12:39:51 -0500

    send

--- src/components/services/smallbusiness/grow.jsx
+++ src/components/services/smallbusiness/grow.jsx
@@ -1,6 +1,6 @@
-const callus_img = '../../../assets/img/logo/callus.png';
-const review_img = '../../../assets/img/service/servicehero.png';
-
+const posts_img = '../../../assets/img/service/posts.png';
+const platform_img = '../../../assets/img/service/platform.png';
+const dart_img = '../../../assets/img/service/dart.png';
 const grow = () => {
     return(
 
@@ -10,15 +10,15 @@ const grow = () => {
   <h2 className='tp-title-md'>What does it take to grow on Tiktok, Reels, and Shorts?</h2>
         
         <div className='perk-container'>
-          <img src={callus_img}></img>
+          <img src={posts_img}></img>
           <h3 className='tp-subtitle-md-white-bold'>Consistent Posts</h3>
         </div>
         <div className='perk-container'>
-          <img src={callus_img}></img>
+          <img src={platform_img}></img>
           <h3 className='tp-subtitle-md-white-bold'>Platform Expertise</h3>
         </div>
         <div className='perk-container'>
-          <img src={callus_img}></img>
+          <img src={dart_img}></img>
 
           <h3 className='tp-subtitle-md-white-bold'>Brand Positioning</h3>
         </div>
