f4fc0cd20ac6e84112405823997927cc416b7ff9
moeayyad8 <68765170+moeayyad8@users.noreply.github.com>
2023-01-07 20:46:49 -0500

    thank you

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Wrapper } from '../layout';
+import { FooterThree, Wrapper } from '../layout';
 import { HeaderThree } from '../layout';
-
+import Breadcrumb from '../components/common/breadcrumb/breadcrumb';
 
 const ThankYou = () => {
 return (
 <Wrapper>
 <HeaderThree/>
-
-<div className="top-padding"></div>
+<Breadcrumb/>
+<div className="top-padding"></div><div className="top-padding"></div><div className="top-padding"></div>
+<div className="top-padding"></div><div className="top-padding"></div><div className="top-padding"></div>
+<h1 className="tp-title-md">Thank You!</h1>
 <h1 className="tp-title-md">Redirecting you home </h1>
+<h1 className='tp-title-md'>...</h1>
+<div className="top-padding"></div><div className="top-padding"></div><div className="top-padding"></div>
+<div className="top-padding"></div><div className="top-padding"></div><div className="top-padding"></div>
+<FooterThree/>
+
 </Wrapper>
 
     )
