c6a449c03054a390bef6d7955bdaf38fb549c7fb
moeayyad8 <68765170+moeayyad8@users.noreply.github.com>
2023-01-08 11:59:48 -0500

    confirmationpage

---
+++ src/pages/confirmation.jsx
@@ -0,0 +1,32 @@
+import { FooterThree, HeaderThree, Wrapper } from "../layout";
+import SEO from "../components/seo";
+import Breadcrumb from "../components/common/breadcrumb/breadcrumb";
+
+
+
+const Confirmation = () => {
+    return (
+        <Wrapper>
+            <SEO pageTitle={'Payment Confirmed'} />
+
+            <HeaderThree />
+            <Breadcrumb />
+            <div className="top-padding"></div><div className="top-padding"></div><div className="top-padding"></div>
+            <div className="top-padding"></div><div className="top-padding"></div><div className="top-padding"></div>
+            <h1 className="tp-title-md"> Welcome to the team!</h1>
+            <h1 className="tp-title-md"> A team member will reach out to you shortly.  </h1>
+            <Link href="/">
+                <div className="row-center"><button href='/#' className="tp-btn">Return Home</button></div>
+            </Link>
+            <div className="row-center"><button href='/#' className="tp-btn">Return Home</button></div>
+            <div className="top-padding"> </div><div className="top-padding"></div><div className="top-padding"></div>
+            <div className="top-padding"> </div><div className="top-padding"></div><div className="top-padding"></div>
+
+            <FooterThree />
+
+
+        </Wrapper>
+    );
+};
+
+export default Confirmation;
